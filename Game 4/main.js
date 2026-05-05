00a1295383397397a8264af257e45b62e65ced5b
Fiona13-tech <anzoofiona5@gmail.com>
2026-05-05 18:16:48 +0300

    First commit

--- Game 4/main.js
+++ Game 4/main.js
@@ -3,30 +3,30 @@ const ctx = canvas.getContext('2d');
 
 //rect 1
 ctx.fillstyle= 'green';
-ctx.fillRect(0,350,50,50);
+ctx.fillRect(0,525,75,75);
 
 //rect 2
 ctx.fillstyle= 'green';
-ctx.fillRect(550,350,50,50);
+ctx.fillRect(825,525,75,75);
 
 //rect 3
 ctx.fillstyle= 'green';
-ctx.fillRect(550,0,50,50);
+ctx.fillRect(825,0,75,75);
 
 //rect 4
 ctx.fillstyle= 'green';
-ctx.fillRect(0,0,50,50);
+ctx.fillRect(0,0,75,75);
 
 //rect 2
 ctx.fillstyle= 'green';
-ctx.fillRect(50,90,200,200);
+ctx.fillRect(75,135,300,300);
 
 //rect 2
 ctx.fillstyle= "green";
-ctx.fillRect(410,130,150,150);
+ctx.fillRect(615,195,225,225);
 
 //rect 4
 ctx.fillstyle= 'green';
-ctx.fillRect(320,170,10,300);
+ctx.fillRect(480,255,15,450);
 
 //player
